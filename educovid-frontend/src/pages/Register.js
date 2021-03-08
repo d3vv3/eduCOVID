f64074c34f7afdc15972c1fff6bd1faf39233d46
d3vv3 <jaimecondel@gmail.com>
2021-03-08 18:45:36 +0100

    enh: added back button and changed GDPR text

--- educovid-frontend/src/pages/Register.js
+++ educovid-frontend/src/pages/Register.js
@@ -1,195 +1,191 @@
 import React, { useState } from "react";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 // Bootstrap imports
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { LinkContainer } from "react-router-bootstrap";
 
 // Local imports
-import PasswordStrength from '../components/PasswordStrength'
+import PasswordStrength from "../components/PasswordStrength";
 
 // External library imports
 import zxcvbn from "zxcvbn";
 
-
 function Register({ history }) {
+  const [centerName, setCenterName] = useState("");
+  const [idNumber, setIdNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [gdprAcceptance, setGdprAcceptance] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [feedbacks, setFeedbacks] = useState({
+    centerName: "",
+    idNumber: "",
+    password: "",
+    gdprAcceptance:
+      "Debe aceptar la Ley General de Protección de Datos Europea para usar nuestros servicios"
+  });
 
-    const [centerName, setCenterName] = useState("");
-    const [idNumber, setIdNumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [gdprAcceptance, setGdprAcceptance] = useState(false);
-    const [errors, setErrors] = useState({});
-    const [feedbacks, setFeedbacks] = useState({
-        centerName: "",
-        idNumber: "",
-        password: "",
-        gdprAcceptance: "Debe aceptar la Ley General de Protección de Datos Europea para usar nuestros servicios",
-    });
+  const validateForm = () => {
+    let errs = {};
+    let msgs = {};
 
-    const validateForm = () => {
-
-        let errs = {};
-        let msgs = {};
-
-        const validateCenterName = () => {
-            // Validate center name field by string length
-            msgs.centerName = "The center name must be at least 4 caracters"
-            errs.centerName = centerName.length < 4
-            // TODO: Check if the center name already exists in the service
-        }
-
-        const validateID = () => {
-            // Validate responsable ID via regex
-            let idNumberRegex = /^[0-9]{8,8}[A-Za-z]$/
-            msgs.idNumber = "El DNI o pasaporte no es válido"
-            errs.idNumber = !idNumberRegex.test(idNumber)
-            // TODO: Check with backend if DNI or passport already in center
-        }
-
-        const validatePassword = () => {
-            // This validation is provided by an open source tool here
-            // https://github.com/dropbox/zxcvbn
-            msgs.password = "La contraseña es demasiado débil"
-            errs.password = zxcvbn(password).score < 2
-        }
-
-        // Run the validations
-        validateCenterName();
-        validateID();
-        validatePassword();
-
-        // Set states only once instead of once per validation
-        setErrors(errs);
-        setFeedbacks(msgs);
-
-        // Return true if there were no errors
-        return !Object.values(errs).some(item => item === true)
-
-    }
-
-    const handleSubmit = (event) => {
-        // Handle submit and prevent the form from submiting to validate
-        event.preventDefault();
-        let valid = validateForm();
-        if (Object.keys(errors).length > 0
-            && valid) {
-            history.push("/");
-            return true;
-        }
-
+    const validateCenterName = () => {
+      // Validate center name field by string length
+      msgs.centerName = "The center name must be at least 4 caracters";
+      errs.centerName = centerName.length < 4;
+      // TODO: Check if the center name already exists in the service
     };
 
-    const updateFormState = (event) => {
-        // On change, set the states with the updates
-        const { name, value } = event.target;
-        switch (name) {
-          case "centerName":
-            setCenterName(value);
-            break;
-          case "idNumber":
-            setIdNumber(value);
-            break;
-          case "password":
-            setPassword(value);
-            break;
-          case "gdprAcceptance":
-            setGdprAcceptance(value);
-            break;
-          default:
-            return;
-        }
+    const validateID = () => {
+      // Validate responsable ID via regex
+      let idNumberRegex = /^[0-9]{8,8}[A-Za-z]$/;
+      msgs.idNumber = "El DNI o pasaporte no es válido";
+      errs.idNumber = !idNumberRegex.test(idNumber);
+      // TODO: Check with backend if DNI or passport already in center
+    };
+
+    const validatePassword = () => {
+      // This validation is provided by an open source tool here
+      // https://github.com/dropbox/zxcvbn
+      msgs.password = "La contraseña es demasiado débil";
+      errs.password = zxcvbn(password).score < 2;
+    };
+
+    // Run the validations
+    validateCenterName();
+    validateID();
+    validatePassword();
+
+    // Set states only once instead of once per validation
+    setErrors(errs);
+    setFeedbacks(msgs);
+
+    // Return true if there were no errors
+    return !Object.values(errs).some(item => item === true);
   };
 
+  const handleSubmit = event => {
+    // Handle submit and prevent the form from submiting to validate
+    event.preventDefault();
+    let valid = validateForm();
+    if (Object.keys(errors).length > 0 && valid) {
+      history.push("/");
+      return true;
+    }
+  };
+
+  const updateFormState = event => {
+    // On change, set the states with the updates
+    const { name, value } = event.target;
+    switch (name) {
+      case "centerName":
+        setCenterName(value);
+        break;
+      case "idNumber":
+        setIdNumber(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "gdprAcceptance":
+        setGdprAcceptance(value);
+        break;
+      default:
+        return;
+    }
+  };
 
   return (
     <div className="register-container">
-        <h1>
-            Registra tu Centro
-        </h1>
+      <h1>Registra tu Centro</h1>
 
-        <div className="register-center-form">
-        <Form
-            onSubmit={handleSubmit.bind(this)}
-            >
-            <Form.Group controlId="formCenterName">
-                <Form.Label>Nombre del centro</Form.Label>
-                <Form.Control
-                    required
-                    autoFocus
-                    type="text"
-                    placeholder="Introduzca el nombre del centro"
-                    name="centerName"
-                    onChange={updateFormState}
-                    value={centerName}
-                    isInvalid={!!errors.centerName}
-                />
-                <Form.Text className="text-muted">
-                    No debe existir en nuestra plataforma
-                </Form.Text>
-                <Form.Control.Feedback type="invalid">
-                    {feedbacks.centerName}
-                </Form.Control.Feedback>
-                <Form.Control.Feedback type="valid">
-                    Perfecto
-                </Form.Control.Feedback>
-            </Form.Group>
+      <div className="register-center-form">
+        <Form onSubmit={handleSubmit.bind(this)}>
+          <Form.Group controlId="formCenterName">
+            <Form.Label>Nombre del centro</Form.Label>
+            <Form.Control
+              required
+              autoFocus
+              type="text"
+              placeholder="Introduzca el nombre del centro"
+              name="centerName"
+              onChange={updateFormState}
+              value={centerName}
+              isInvalid={!!errors.centerName}
+            />
+            <Form.Text className="text-muted">
+              No debe existir en nuestra plataforma
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              {feedbacks.centerName}
+            </Form.Control.Feedback>
+            <Form.Control.Feedback type="valid">Perfecto</Form.Control.Feedback>
+          </Form.Group>
 
-            <Form.Group controlId="formAdminID">
-                <Form.Label>ID del Responsable COVID</Form.Label>
-                <Form.Control
-                    required
-                    type="text"
-                    name="idNumber"
-                    placeholder="Introduzca un DNI o pasaporte"
-                    onChange={updateFormState}
-                    value={idNumber}
-                    isInvalid={!!errors.idNumber}
-                />
-                <Form.Text className="text-muted">
-                    Debe ser único para el centro
-                </Form.Text>
-                <Form.Control.Feedback type="invalid">
-                    {feedbacks.idNumber}
-                </Form.Control.Feedback>
-            </Form.Group>
+          <Form.Group controlId="formAdminID">
+            <Form.Label>ID del Responsable COVID</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              name="idNumber"
+              placeholder="Introduzca un DNI o pasaporte"
+              onChange={updateFormState}
+              value={idNumber}
+              isInvalid={!!errors.idNumber}
+            />
+            <Form.Text className="text-muted">
+              Debe ser único para el centro
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              {feedbacks.idNumber}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control
-                    required
-                    type="password"
-                    name="password"
-                    placeholder="Introduzca una contraseña"
-                    onChange={updateFormState}
-                    value={password}
-                    isInvalid={!!errors.password}
-                />
-                <PasswordStrength password={password}/>
-                <Form.Control.Feedback type="invalid">
-                    {feedbacks.password}
-                </Form.Control.Feedback>
-            </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              name="password"
+              placeholder="Introduzca una contraseña"
+              onChange={updateFormState}
+              value={password}
+              isInvalid={!!errors.password}
+            />
+            <PasswordStrength password={password} />
+            <Form.Control.Feedback type="invalid">
+              {feedbacks.password}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <Form.Group controlId="formGDPRCheckbox">
-                <Form.Check
-                    required
-                    type="checkbox"
-                    label={"Acepto la Ley General de Protección de Datos Europea"}
-                    feedback={feedbacks.gdprAcceptance}
-                    isInvalid={!!errors.gdprAcceptance}
-                />
-            </Form.Group>
-
+          <Form.Group controlId="formGDPRCheckbox">
+            <Form.Check
+              required
+              type="checkbox"
+              label={
+                "Acepto el tratamiento de los datos del centro así como \
+                  sus alumnos y personas adscritas conforme con el Reglamento \
+                  General de Protección de Datos Europeo."
+              }
+              feedback={feedbacks.gdprAcceptance}
+              isInvalid={!!errors.gdprAcceptance}
+            />
+          </Form.Group>
+          <div className="buttons-container">
+            <LinkContainer to="/">
+              <Button className="nord-button" variant="primary">
+                Atrás
+              </Button>
+            </LinkContainer>
             <Button className="nord-button" variant="primary">
-                Enviar
+              Enviar
             </Button>
+          </div>
         </Form>
-        </div>
+      </div>
 
-        <div className="register-form-admin">
-
-        </div>
-
+      <div className="register-form-admin"></div>
     </div>
   );
 }
