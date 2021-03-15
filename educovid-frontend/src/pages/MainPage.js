9c519745e5582c0e06f9bd5d13c019b74ca9b1be
d3vv3 <jaimecondel@gmail.com>
2021-03-15 15:12:17 +0100

    enh: updated logo and styling

--- educovid-frontend/src/pages/MainPage.js
+++ educovid-frontend/src/pages/MainPage.js
@@ -1,45 +1,44 @@
 import React from "react";
 
 // Bootstrap imports
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { LinkContainer } from "react-router-bootstrap";
 
 function MainPage(props) {
   return (
     <div className="main-page-container">
+      <div className="centered-div">
+        <img
+          className="logo"
+          src={process.env.PUBLIC_URL + "logo512.png"}
+          alt="Shield from COVID logotype"
+        />
+        <h1>eduCOVID</h1>
 
-        <div className="centered-div">
-            <img
-                className="logo"
-                src={process.env.PUBLIC_URL + "assets/logo.png"}
-                alt="Shield from COVID logotype"
-            />
-            <h1>eduCOVID</h1>
+        <p className="description">
+          Una herramienta para seguimiento y control del COVID en los centros
+          educativos.
+        </p>
 
-            <p className="description">
-                Una herramienta para seguimiento y control del COVID en los centros
-                educativos.
-            </p>
+        <p className="tagline">Para estudiantes, por estudiantes.</p>
 
-            <p className="tagline">
-                Para estudiantes, por estudiantes.
-            </p>
-
-            <div className="buttons-container">
-                {!props.loggedIn ? <LinkContainer to='/register'>
-                    <Button
-                        type="submit" variant="primary" className="nord-button">
-                        Registra tu centro
-                    </Button>
-                </LinkContainer> : null}
-                {!props.loggedIn ? <LinkContainer to='/login'>
-                    <Button
-                        type="submit" variant="primary" className="nord-button">
-                        Accede
-                    </Button>
-                </LinkContainer> : null}
-            </div>
+        <div className="buttons-container">
+          {!props.loggedIn ? (
+            <LinkContainer to="/register">
+              <Button type="submit" variant="primary" className="nord-button">
+                Registra tu centro
+              </Button>
+            </LinkContainer>
+          ) : null}
+          {!props.loggedIn ? (
+            <LinkContainer to="/login">
+              <Button type="submit" variant="primary" className="nord-button">
+                Accede
+              </Button>
+            </LinkContainer>
+          ) : null}
         </div>
+      </div>
     </div>
   );
 }
