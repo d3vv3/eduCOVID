3e9446d5b1919b00df7a84ea48ec69d581b0e6d8
Javier Abejaro Capilla <>
2021-05-06 01:16:20 +0200

    rest para crear, modificar y borrar grupos hechos a falta de probar con postman

--- educovid-backend/educovid-backend/src/main/java/es/upm/dit/isst/educovid/rest/GrupoBurbujaResource.java
+++ educovid-backend/educovid-backend/src/main/java/es/upm/dit/isst/educovid/rest/GrupoBurbujaResource.java
@@ -1,5 +1,7 @@
 package es.upm.dit.isst.educovid.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,7 +9,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,6 +24,7 @@ import es.upm.dit.isst.educovid.anotation.Secured;
 import es.upm.dit.isst.educovid.aux.Security;
 import es.upm.dit.isst.educovid.dao.AlumnoDAOImpl;
 import es.upm.dit.isst.educovid.dao.CentroEducativoDAOImpl;
+import es.upm.dit.isst.educovid.dao.ClaseDAOImpl;
 import es.upm.dit.isst.educovid.dao.GrupoBurbujaDAOImpl;
 import es.upm.dit.isst.educovid.dao.ProfesorDAOImpl;
 import es.upm.dit.isst.educovid.model.Alumno;
@@ -90,4 +97,50 @@ public class GrupoBurbujaResource {
 		GrupoBurbuja gNew = new GrupoBurbuja(g.getNombre(), g.getEstadoSanitario(), null, null, null, null);
 		return Response.ok(gNew, MediaType.APPLICATION_JSON).build();
 	}
+	
+	//Métodos rest para gestionar centro parte de grupos
+	
+	@POST
+	@Secured
+	@Path("/{classId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createGroup(GrupoBurbuja grupoNuevo, @PathParam("classId") String classId) throws URISyntaxException {
+		Clase c = ClaseDAOImpl.getInstance().readClasebyId(classId);
+		GrupoBurbuja g = GrupoBurbujaDAOImpl.getInstance().createGrupoBurbuja(grupoNuevo);
+		List<GrupoBurbuja> grupoList = c.getGruposBurbuja();
+		grupoList.add(g);
+		c.setGruposBurbuja(grupoList);
+	    if (g != null && grupoList != null) {
+	            URI uri = new URI("/educovid-backend/rest/grupo/" + g.getId());
+	            return Response.created(uri).build();
+	    }
+	    return Response.status(Response.Status.NOT_FOUND).build();
+	}
+	
+	
+	@DELETE
+	@Secured
+	@Path("/{id}")
+	public Response deleteGroup(@PathParam("id") String id) {
+		GrupoBurbuja g = GrupoBurbujaDAOImpl.getInstance().readGrupoBurbujabyId(id);
+		if (g == null)
+			return Response.notModified().build();
+		GrupoBurbujaDAOImpl.getInstance().deleteGrupoBurbuja(g);
+		return Response.ok(MediaType.APPLICATION_JSON).build();
+	}
+	
+	@PUT
+	@Secured
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response updateGroup(@PathParam("id") String id, GrupoBurbuja g) {
+		GrupoBurbuja gr = GrupoBurbujaDAOImpl.getInstance().readGrupoBurbujabyId(id);
+	    if (gr == null) {
+	    	return Response.notModified().build();
+	    }
+	    GrupoBurbujaDAOImpl.getInstance().updateGrupoBurbuja(gr);
+	    return Response.ok(gr, MediaType.APPLICATION_JSON).build();
+	}
+	
 }
